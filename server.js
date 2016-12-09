9ea9f4ffe86a720b7386cf610e73b160d902ddc7
kadams277 <kadams277@gmail.com>
2016-12-09 14:09:57 -0600

    Fixin more stuff, sighhh

--- server.js
+++ server.js
@@ -17,23 +17,25 @@ app.set("view engine", "handlebars");
 
 var mysql = require("mysql");
 
+/*
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
   database: "wishes_db"
 });
+*/
 
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-  host : "localhost",
-  user: "root",
-  password: "root",
-  database: "wishes_db"
-});
-}; 
+    host : "localhost",
+    user: "root",
+    password: "root",
+    database: "wishes_db"
+  });
+};
 
 
 connection.connect(function(err) {
