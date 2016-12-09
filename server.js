30a31a20562640462c026c3f41f045b046556525
kadams277 <kadams277@gmail.com>
2016-12-09 13:23:02 -0600

    JAWS DB code

--- server.js
+++ server.js
@@ -24,6 +24,18 @@ var connection = mysql.createConnection({
   database: "wishes_db"
 });
 
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+  host : "localhost",
+  user: "root",
+  password: "root",
+  database: "wishes_db"
+});
+}; 
+
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
